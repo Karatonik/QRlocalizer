8c6fe3e3406088b5eb8781bbe04abd75374a0c99
Karaton <mat.kalksztejn@wp.pl>
2022-07-01 10:57:10 +0200

    update

--- app/src/main/java/pl/r/mmdd_pum_projekt/MainActivity.java
+++ app/src/main/java/pl/r/mmdd_pum_projekt/MainActivity.java
@@ -55,18 +55,10 @@ public class MainActivity extends AppCompatActivity {
         Button permBtn = findViewById(R.id.bt_permission);
 
         permBtn.setOnClickListener(v -> {
-            if (ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-            ) {
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
                                 , Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE},
-                        3);
-            }
+                        5);
         });
 
 
